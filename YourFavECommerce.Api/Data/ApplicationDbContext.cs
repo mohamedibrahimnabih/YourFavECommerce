75b8bfe8bc26d364c49476008e6677b6ee599f73
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-03-16 15:48:47 +0200

    Configure DB

---
+++ YourFavECommerce.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿ using Microsoft.EntityFrameworkCore;
+using YourFavECommerce.Api.Models;
+
+namespace YourFavECommerce.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
