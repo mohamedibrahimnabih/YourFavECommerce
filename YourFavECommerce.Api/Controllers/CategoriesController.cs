d5f94eb02693f7956a9dd32e35536390555ec0fc
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-03-16 17:08:18 +0200

    CRU(D) Operations

---
+++ YourFavECommerce.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YourFavECommerce.Api.Data;
+using YourFavECommerce.Api.Models;
+
+namespace YourFavECommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            var categories = _context.Categories.ToList();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) 
+        {
+            var categroy = _context.Categories.Find(id);
+
+            //if (categroy == null)
+            //    return NotFound();
+
+            //return Ok(categroy);
+
+            return categroy == null ?  NotFound() : Ok(categroy);
+        }
+
+        [HttpPost("")]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            //return Created($"{Request.Scheme}://{Request.Host}/api/Categories/{category.Id}", category);
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, Category category)
+        {
+            var categroyInDb = _context.Categories.AsNoTracking().FirstOrDefault(e => e.Id == id);
+
+            if (categroyInDb != null)
+            {
+                category.Id = id;
+
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
